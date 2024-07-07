99769c7d9b6a8325e76f35ca085af833302623e3
e. lim <88494428+kuehbiko@users.noreply.github.com>
2024-07-07 13:35:02 +0800

    Update emojize.py

--- Harvard CS50P Introduction to Programming with Python/Problem Set 4 - Libraries/emojize.py
+++ Harvard CS50P Introduction to Programming with Python/Problem Set 4 - Libraries/emojize.py
@@ -3,7 +3,7 @@
 # input: In a file called emojize.py, implement a program that prompts the user for a str in English and then
 # output: outputs the “emojized” version of that str, converting any codes (or aliases) therein to their corresponding emoji.
 
-import emoji
+import emoji # https://github.com/carpedm20/emoji
 
 text = input("Input: ")
 print(f"Output: {emoji.emojize(text, language='alias')}")
